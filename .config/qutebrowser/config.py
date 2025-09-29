f83612e8f4004778032a1c294ad696fb5373d79a
ae00 <ae00@users.noreply.github.com>
2025-09-29 20:10:25 +0200

    Added streamlink hint command to qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,6 +3,7 @@ config.bind(',I', 'hint images download')
 config.bind(',i', 'hint images spawn feh {hint-url}')
 
 config.bind('<Ctrl+Shift+d>', 'hint links spawn --detach mpv {hint-url}')
+config.bind('<Ctrl+Shift+s>', 'hint links spawn streamlink -p mpv {hint-url} best')
 
 config.set("fileselect.handler", "external")
 config.set("fileselect.single_file.command", ["alacritty", "-e", "ranger", "--choosefile={}"])
